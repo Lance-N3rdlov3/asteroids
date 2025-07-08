5d2610778eed1793ba71e934e79ae8eac34593dd
Lance <N3rdlov3@n3rdlabs.dev>
2025-07-08 11:54:26 -0400

    added groups, and implimented logic for asteroids and collision detection

--- main.py
+++ main.py
@@ -1,26 +1,52 @@
-import pygame 
+import sys
+import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
 
-    bg_color = [0,0,0]
+    bg_color = [0, 0, 0]
+
     clock = pygame.time.Clock()
+
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game Over")
+                sys.exit()
+
         screen.fill("black")
-        player.update(dt) 
-        player.draw(screen) 
-        
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
